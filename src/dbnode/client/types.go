b74d2f4f6e0f4cb201e103c597074bcc1cad7594
Simon Robb <srobb@uber.com>
2019-08-06 15:30:24 -0400

    WIP added client opts support for multiple clusters

--- src/dbnode/client/types.go
+++ src/dbnode/client/types.go
@@ -228,12 +228,6 @@ type Options interface {
 	// InstrumentOptions returns the instrumentation options
 	InstrumentOptions() instrument.Options
 
-	// SetTopologyInitializer sets the TopologyInitializer
-	SetTopologyInitializer(value topology.Initializer) Options
-
-	// TopologyInitializer returns the TopologyInitializer
-	TopologyInitializer() topology.Initializer
-
 	// SetReadConsistencyLevel sets the read consistency level
 	SetReadConsistencyLevel(value topology.ReadConsistencyLevel) Options
 
@@ -475,6 +469,37 @@ type Options interface {
 	ReaderIteratorAllocate() encoding.ReaderIteratorAllocate
 }
 
+type BarOptions interface {
+	// SetTopologyInitializers sets the TopologyInitializers
+	SetTopologyInitializers(value []topology.Initializer) Options
+
+	// TopologyInitializers returns the TopologyInitializers
+	TopologyInitializers() []topology.Initializer
+
+	// SetAsyncTopologyInitializers sets the TopologyInitializers
+	SetAsyncTopologyInitializers(value []topology.Initializer) Options
+
+	// AsyncTopologyInitializers returns the TopologyInitializers
+	AsyncTopologyInitializers() []topology.Initializer
+}
+
+type MultiOptions interface {
+	Options
+	BarOptions
+}
+
+type FooOptions interface {
+	// SetTopologyInitializer sets the TopologyInitializer
+	SetTopologyInitializer(value topology.Initializer) Options
+
+	// TopologyInitializer returns the TopologyInitializer
+	TopologyInitializer() topology.Initializer
+}
+type SingleOptions interface {
+	Options
+	FooOptions
+}
+
 // AdminOptions is a set of administration client options
 type AdminOptions interface {
 	Options
@@ -526,6 +551,16 @@ type AdminOptions interface {
 
 	// StreamBlocksRetrier returns the retrier for streaming blocks
 	StreamBlocksRetrier() xretry.Retrier
+}
+
+type AdminMultiOptions interface {
+	AdminOptions
+	BarOptions
+}
+
+type AdminSingleOptions interface {
+	AdminOptions
+	FooOptions
 }
 
 // The rest of these types are internal types that mocks are generated for
